9fe4acbd4acc4239f1d1ed064c621703ca584cbd
骇人的籽 <60039041+JoshuaYu-crash@users.noreply.github.com>
2020-03-06 23:37:24 +0800

    修改第二次作业部分代码

--- 2.4(含解题说明).cpp
+++ 2.4(含解题说明).cpp
@@ -1,17 +1,28 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int a[300005];
 int main() {
-	int n, sign = 0;
+	int n, sign = 1;
 	cin >> n;
-	for(int i = 1; i <= n; i++) {
-		cin >> a[n];
+	for(int i = 0; i < n; i++) {
+		cin >> a[i];
 	}
-	for(int i = 1; i <= n; i++) {
-		if(a[i] < i && a[i] <= n - i) {
-			cout << "No";
-			return 0;
-		}
-	}
-	cout << "Yes";
+	for(int i = 1; i <= (n - 1) / 2; i++) {
+        if(n % 2 == 0 && i == (n - 1) / 2) {
+            if((a[i] < i && a[n - i - 1] < i + 1) || (a[i] < i + 1 && a[n - i - 1] < i)) {
+                cout << "No";
+                return 0;
+                sign = 0;
+            }
+        }
+        if(a[i] < i || a[n - i - 1] < i) {
+            cout << "No";
+            return 0;
+            sign = 0;
+        }
+    }
+    if(sign) {
+        cout << "Yes";
+    }
 }
